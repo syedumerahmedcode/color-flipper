02525c1898bbbc49926206884e10aeeb3adecb10
syedumerahmedcode <syed.umer.ahmed.code@gmail.com>
2022-06-26 11:21:49 +0200

    Feature: Changing the background color to the 2nd index object in the colors array.

--- app.js
+++ app.js
@@ -2,5 +2,9 @@ const colors = ["green", "red", "rgba(133,122,200)", "#f15025"];
 const btn = document.getElementById("btn");
 const color = document.querySelector(".color");
 btn.addEventListener("click", function () {
-  console.log(document.body);
+  //   console.log(document.body);
+  // get random number between 0 and 3
+  const randomNumber = 2;
+  document.body.style.backgroundColor = colors[randomNumber];
+  color.textContent = colors[randomNumber];
 });
